e92591b1953af922dac7ca29adef930ce4a517c5
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-12-27 12:55:42 +0530

    GET_POST _added

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"example.com/rest-api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Print("Bloodborne")
+	server := gin.Default()
+	server.GET("/events", getEvents) //GET, POST, PUT, DELETE, PATCH
+	server.POST("/events", createEvent)
+
+	server.Run(":6969")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
